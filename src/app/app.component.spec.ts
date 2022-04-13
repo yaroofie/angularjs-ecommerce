8a18893a54a70a01971970e831f01a8825c22fe4
yaroofie <yaroofie@gmail.com>
2022-04-13 08:35:27 +0430

    changed hello-world to ecommerce

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-hello-world'`, () => {
+  it(`should have as title 'angular-ecommerce'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-hello-world');
+    expect(app.title).toEqual('angular-ecommerce');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-hello-world app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-ecommerce app is running!');
   });
 });
