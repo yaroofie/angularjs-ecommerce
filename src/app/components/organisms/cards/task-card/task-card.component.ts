17fbbac0a967008320399871aca9a680d0262206
yaroofie <yaroofie@gmail.com>
2022-04-14 10:12:40 +0430

    updated task-card

--- src/app/components/organisms/cards/task-card/task-card.component.ts
+++ src/app/components/organisms/cards/task-card/task-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { ITask } from '../../../../Contracts/ITask';
 
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
-  styleUrls: ['./task-card.component.scss']
+  styleUrls: ['./task-card.component.scss'],
 })
 export class TaskCardComponent implements OnInit {
+  @Input() task?: ITask;
+  selected: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSelect(value: boolean) {
+    this.selected = value;
   }
 
+  onDelete (){
+    console.log('delete', this.task);
+  }
 }
--- src/app/components/pages/panel/tasks/tasks.component.ts
+++ src/app/components/pages/panel/tasks/tasks.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { TASKS } from "../../../../mock-tasks";
+import { ITask } from "../../../../Contracts/ITask";
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
-
+  tasks : ITask[] = TASKS;
   constructor() { }
 
   ngOnInit(): void {
